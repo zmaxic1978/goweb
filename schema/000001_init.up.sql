6eea2da65a10797e2dfbccba76fac78bca4bfb3e
zmaxic_vs@hotmail.com <zmaxic_vs@hotmail.com>
2024-06-15 19:02:31 +0300

    commit

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -24,5 +24,27 @@ CREATE TABLE books
     isbn     varchar(255) not null
 );
 
+INSERT INTO authors (firstname, lastname, description, birthday) VALUES ('Агата', 'Кристи', 'детективщица!', '1890-09-15');
+COMMIT;
 INSERT INTO authors (firstname, lastname, description, birthday) VALUES ('Артур', 'Конан Дойл', 'детективщик', '1859-05-22');
+COMMIT;
+INSERT INTO authors (firstname, lastname, description, birthday) VALUES ('Стивен', 'Кинг', 'автор ужастиков', '1947-09-21');
+COMMIT;
+INSERT INTO authors (firstname, lastname, description, birthday) VALUES ('Айзек', 'Азимов', 'современный писатель-фантаст', '1919-04-06');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Убийство в доме викария', 1, 1954, '123-343-5465');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Смерть на Ниле', 1, 1951, '123-343-5466');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Собака Баскервиллей', 2, 1904, '56-343-5465');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Этюд в багровых тонах', 2, 1905, '56-343-5466');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Оно', 3, 1986, '985-13-6269-7');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Мертвая зона', 3, 1991, '991-15-4544-2');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Я робот', 4, 1950, '985-13-6269-7');
+COMMIT;
+INSERT INTO books (name, authorid, year, isbn) VALUES ('Сами боги', 4, 1972, '978-5-04-172716-1');
 COMMIT;
